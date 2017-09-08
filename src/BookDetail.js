0c4f9374150f71d6b7bfbc9a0e23016d917aa01a
inkymaze <nickwcannon@gmail.com>
2017-09-08 12:20:34 -0700

    add book pre-selection to detail component

--- src/BookDetail.js
+++ src/BookDetail.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 
 class BookDetail extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  
 
   render () {
     let authors = this.props.book.authors || [];
-    let shelf = this.props.book.authors || [];
+    let shelf = this.props.book.shelf || [];
+
     return (
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.smallThumbnail})` }}></div>
           <div className="book-shelf-changer">
-            <select>
+            <select value={shelf}>
               <option value="none" disabled>Move to...</option>
 
               <option value="currentlyReading">Currently Reading</option>
